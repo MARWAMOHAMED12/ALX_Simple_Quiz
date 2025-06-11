77afed8d4200badb5e371eb8e0b00c501f2416d4
MARWAMOHAMED12 <5849hamada@gmail.com>
2025-06-11 18:45:55 +0300

    update : quiz.js

--- quiz.js
+++ quiz.js
@@ -1,24 +1,19 @@
-// Function to check the user's answer
 function checkAnswer() {
-    const correctAnswer ="4":
-    const selectedoption = document.querySelector('input [name="quiz"]: checked');
-    if (Selecteboption) {
-        const userAnswer = selectedoption.value;
-        const Feedback = document.getElementById("feedback");
-        if (userAnser === correctAnswer) {
-            feedback.textcontent = "correct! well done!";
+    const correctAnswer = "4";
+    const selectedOption = document.querySelector('input[name="quiz"]:checked');
 
+    if (selectedOption) {
+        const userAnswer = selectedOption.value;
+        const feedback = document.getElementById("feedback");
+
+        if (userAnswer === correctAnswer) {
+            feedback.textContent = "Correct! Well done.";
         } else {
-            feedback.textcontent = "That's incoerrect. Try again!";
-
-        } else { 
-            document.getElementById("feedback").textContent ="please select an answer.";
-
+            feedback.textContent = "That's incorrect. Try again!";
         }
-        document.getElementById("submit-answer").addEventListener("click", checkAnswer);
-
-        
-           
+    } else {
+        document.getElementById("feedback").textContent = "Please select an answer.";
     }
-
 }
+
+document.getElementById("submit-answer").addEventListener("click", checkAnswer);
